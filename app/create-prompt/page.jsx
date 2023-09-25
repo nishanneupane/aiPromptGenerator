8380267e867c715dd3d7bc90af6d4f7cd1fa7de5
unknown <nishanneupane368@gmail.com>
2023-09-25 17:27:39 +0545

    updated

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,13 +2,17 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
+
+  if(!session){
+    return redirect("/")
+  }
 
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
